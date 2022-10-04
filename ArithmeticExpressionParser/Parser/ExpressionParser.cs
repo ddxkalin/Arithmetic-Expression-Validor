6447f64132355ad218fddc390998d3302c4b72db
Kalin Stoev <kstoev9316@gmail.com>
2022-10-05 01:00:05 +0300

    Parser Logic Done / Test WIP /

---
+++ ArithmeticExpressionParser/Parser/ExpressionParser.cs
@@ -0,0 +1,95 @@
+﻿namespace ArithmeticExpressionParser.Parser
+{
+    using ArithmeticExpressionParser.Model;
+    using ArithmeticExpressionParser.Exceptions;
+    using System.ComponentModel;
+
+    public class ExpressionParser
+    {
+        private Tokenizer _tokenizer;
+
+        public Tuple<IExpression, List<ParsingException>> Parse(string s)
+        {
+            _tokenizer = new Tokenizer(s);
+            var result = ParseAddSubtract();
+            return new Tuple<IExpression, List<ParsingException>>(result, _tokenizer.GetErrors());
+        }
+
+        private IExpression ParseUnary()
+        {
+            while (true)
+            {
+                IExpression res = new ErrorExpression();
+                switch (_tokenizer.GetNextToken())
+                {
+                    case Token.Number:
+                        res = new Number(_tokenizer.GetValue());
+                        _tokenizer.GetNextToken();
+                        break;
+                    case Token.Negate:
+                        res = new Negate(ParseUnary());
+                        break;
+                    case Token.OpenParenthesis:
+                        res = ParseAddSubtract();
+                        if (_tokenizer.GetCurrentToken() != Token.CloseParenthesis)
+                        {
+                            _tokenizer.AddException(new MissingCloseParenthesisException(
+                                _tokenizer.GetExpression(),
+                                _tokenizer.GetInd()));
+                        }
+
+                        _tokenizer.GetNextToken();
+                        break;
+                    case Token.Error:
+                        continue;
+                }
+
+                return res;
+            }
+        }
+
+        private IExpression ParseMultiplyDivide()
+        {
+            var res = ParseUnary();
+            while (true)
+            {
+                switch (_tokenizer.GetCurrentToken())
+                {
+                    case Token.Multiply:
+                        res = new Multiply(res, ParseUnary());
+                        break;
+                    case Token.Divide:
+                        res = new Divide(res, ParseUnary());
+                        break;
+                    case Token.Error:
+                        _tokenizer.GetNextToken();
+                        break;
+                    default:
+                        return res;
+                }
+            }
+        }
+
+        private IExpression ParseAddSubtract()
+        {
+            var res = ParseMultiplyDivide();
+            while (true)
+            {
+                switch (_tokenizer.GetCurrentToken())
+                {
+                    case Token.Add:
+                        res = new Add(res, ParseMultiplyDivide());
+                        break;
+                    case Token.Subtract:
+                        res = new Subtract(res, ParseMultiplyDivide());
+                        break;
+                    case Token.Error:
+                        _tokenizer.GetNextToken();
+                        break;
+                    default:
+                        return res;
+                }
+            }
+        }
+    }
+}
---
+++ ArithmeticExpressionParser/Parser/Tokenizer.cs
@@ -0,0 +1,242 @@
+﻿namespace ArithmeticExpressionParser.Parser
+{
+    using ArithmeticExpressionParser.Exceptions;
+
+    public class Tokenizer
+    {
+        private readonly List<ParsingException> _errors;
+        private readonly string _expression;
+        private Token _currentToken;
+        private int _ind;
+        private int _parenthesisBalance;
+
+        private double _value;
+
+        public Tokenizer(string expression)
+        {
+            _errors = new List<ParsingException>();
+            _expression = expression;
+            _ind = 0;
+            _parenthesisBalance = 0;
+            _currentToken = Token.Begin;
+        }
+
+        public int GetInd()
+        {
+            return _ind;
+        }
+
+        public string GetExpression()
+        {
+            return _expression;
+        }
+
+        public List<ParsingException> GetErrors()
+        {
+            return _errors;
+        }
+
+        public void AddException(ParsingException e)
+        {
+            _errors.Add(e);
+        }
+
+        public Token GetCurrentToken()
+        {
+            return _currentToken;
+        }
+
+        public Token GetNextToken()
+        {
+            NextToken();
+            return _currentToken;
+        }
+
+        public double GetValue()
+        {
+            return _value;
+        }
+
+        private void SkipWhiteSpaces()
+        {
+            while (_ind < _expression.Length && char.IsWhiteSpace(_expression[_ind]))
+            {
+                _ind++;
+            }
+        }
+
+        private string GetNumber()
+        {
+            var l = _ind;
+            while (_ind < _expression.Length &&
+                   (_expression[_ind] == '.' || char.IsDigit(_expression[_ind])))
+            {
+                _ind++;
+            }
+
+            var r = _ind;
+            _ind--;
+            return _expression.Substring(l, r - l);
+        }
+
+        private void CheckForArgument()
+        {
+            if (_currentToken != Token.OpenParenthesis && _currentToken != Token.Begin && !_currentToken.IsOperation())
+            {
+                return;
+            }
+
+            _errors.Add(new MissingArgumentException(_expression, _ind));
+        }
+
+        private bool CheckForOperation()
+        {
+            if (_currentToken != Token.CloseParenthesis && _currentToken != Token.Number)
+            {
+                return true;
+            }
+
+            _errors.Add(new MissingOperationException(_expression, _ind));
+            return false;
+        }
+
+        private void NextToken()
+        {
+            if (_currentToken == Token.End)
+            {
+                return;
+            }
+
+            SkipWhiteSpaces();
+            if (_ind >= _expression.Length)
+            {
+                CheckForArgument();
+                _currentToken = Token.End;
+                return;
+            }
+
+            var c = _expression[_ind];
+            switch (c)
+            {
+                case '-':
+                    if (_currentToken == Token.Number || _currentToken == Token.CloseParenthesis || _currentToken == Token.Error)
+                    {
+                        _currentToken = Token.Subtract;
+                    }
+                    else
+                    {
+                        if (_ind + 1 >= _expression.Length)
+                        {
+                            _errors.Add(new MissingArgumentException(_expression, _ind + 1));
+                            _currentToken = Token.Error;
+                        }
+                        else if (char.IsDigit(_expression[_ind + 1]))
+                        {
+                            _ind++;
+                            var begin = _ind;
+                            var firstDigit = _expression[_ind];
+                            var tmp = GetNumber();
+
+                            try
+                            {
+                                _value = double.Parse("-" + tmp);
+                            }
+                            catch (FormatException)
+                            {
+                                _errors.Add(new NumberParsingException(_expression, begin, _ind + 1));
+                                _value = firstDigit;
+                            }
+
+                            _currentToken = Token.Number;
+                        }
+                        else
+                        {
+                            _currentToken = Token.Negate;
+                        }
+                    }
+                    break;
+
+                case '+':
+                    CheckForArgument();
+                    _currentToken = Token.Add;
+                    break;
+
+                case '*':
+                    CheckForArgument();
+                    _currentToken = Token.Multiply;
+                    break;
+
+                case '/':
+                    CheckForArgument();
+                    _currentToken = Token.Divide;
+                    break;
+
+                case '(':
+                    if (_currentToken == Token.CloseParenthesis || _currentToken == Token.Number)
+                    {
+                        _errors.Add(new UnexpectedOpenParenthesisException(_expression, _ind));
+                    }
+                    else
+                    {
+                        _parenthesisBalance++;
+                        _currentToken = Token.OpenParenthesis;
+                    }
+
+                    break;
+
+                case ')':
+                    if (_currentToken == Token.OpenParenthesis || _currentToken.IsOperation())
+                    {
+                        _errors.Add(new MissingArgumentException(_expression, _ind));
+                    }
+
+                    if (_parenthesisBalance - 1 < 0)
+                    {
+                        _errors.Add(new UnexpectedCloseParenthesisException(_expression, _ind));
+                        _currentToken = Token.Error;
+                    }
+                    else
+                    {
+                        _parenthesisBalance--;
+                        _currentToken = Token.CloseParenthesis;
+                    }
+
+                    break;
+
+                default:
+                    if (char.IsDigit(c))
+                    {
+                        if (CheckForOperation())
+                        {
+                            var begin = _ind;
+                            var tmp = GetNumber();
+                            try
+                            {
+                                _value = double.Parse(tmp);
+                            }
+                            catch (FormatException)
+                            {
+                                _errors.Add(new NumberParsingException(_expression, begin, _ind + 1));
+                                _value = int.Parse(c.ToString());
+                            }
+
+                            _currentToken = Token.Number;
+                        }
+                        else
+                        {
+                            _currentToken = Token.Error;
+                        }
+                    }
+                    else
+                    {
+                        _errors.Add(new UnexpectedTokenException(_expression, _ind));
+                        _currentToken = Token.Error;
+                    }
+
+                    break;
+            }
+
+            _ind++;
+        }
+    }
+}
