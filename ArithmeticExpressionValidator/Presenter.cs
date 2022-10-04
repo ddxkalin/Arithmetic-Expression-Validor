8fb2d74550522934c33e30b7162ed2d1a9d7ab88
Kalin Stoev <kstoev9316@gmail.com>
2022-10-04 23:41:54 +0300

    Console Logic Added // Parser/Token WIP //

---
+++ ArithmeticExpressionValidator/Presenter.cs
@@ -0,0 +1,57 @@
+﻿namespace ArithmeticExpressionValidator
+{
+    internal static class Presenter
+    {
+        private static void Main()
+        {
+            using var solver = new Solver();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("==========================================================");
+            Console.WriteLine("***      Arithmetic Expression has been launched       ***");
+            Console.WriteLine("===____________________________________________________===");
+            Console.WriteLine("*** To evaluate an expression, write it to the console ***");
+            Console.WriteLine("===                  and press Enter.                  ===");
+            Console.WriteLine("***----------------------------------------------------***");
+            Console.WriteLine("===      If you want to close Arithmetic Expression,   ===");
+            Console.WriteLine("***      write 'q' to the console, and press Enter     ***");
+            Console.WriteLine("==========================================================");
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("                                                          ");
+            Console.ResetColor();
+
+            while (true)
+            {
+                Console.Write("Enter Expression: ");
+                var request = Console.ReadLine();
+                if (request == "q")
+                {
+                    Console.ResetColor();
+                    break;
+                }
+
+                var (answer, expression, errors) = solver.SendAndEval(request);
+
+                if (errors.Count == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Calculated Result: ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(answer);
+                    Console.WriteLine();
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("Invalid Expression !!!");
+                    Console.ResetColor();
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("**********************************************************");
+            }
+        }
+    }
+}
--- ArithmeticExpressionValidator/Solver.cs
+++ ArithmeticExpressionValidator/Solver.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Text;
-
-namespace ArithmeticExpressionValidator
+﻿namespace ArithmeticExpressionValidator
 {
+    using ArithmeticExpressionParser.Parser;
+    using ParserResult =
+        System.Tuple<double, ArithmeticExpressionParser.Model.IExpression,
+            System.Collections.Generic.List<ArithmeticExpressionParser.Exceptions.ParsingException>>;
+
     internal class Solver : IDisposable
     {
         private readonly ExpressionParser _parser;
@@ -17,7 +19,97 @@ namespace ArithmeticExpressionValidator
             _worker.Start();
         }
 
+        public ParserResult SendAndEval(string s)
+        {
+            var result = new Result();
+            lock (_tasks)
+            {
+                _tasks.Enqueue(new Task((() => { result.Eval(s, _parser); })));
+                Monitor.PulseAll(_tasks);
+            }
 
+            return result.GetRes();
+        }
+
+        private class Result
+        {
+            private ParserResult _res;
+
+            internal Result()
+            {
+                _res = null;
+            }
+
+            internal void Eval(string s, ExpressionParser parser)
+            {
+                lock (this)
+                {
+                    var (expression, errors) = parser.Parse(s);
+                    _res = new ParserResult(expression.Evaluate(), expression, errors);
+                    Monitor.Pulse(this);
+                }
+            }
+
+            internal ParserResult GetRes()
+            {
+                lock (this)
+                {
+                    while (_res == null)
+                    {
+                        Monitor.Wait(this);
+                    }
+                }
+
+                return _res;
+            }
+        }
+
+        private void Eval()
+        {
+            Task task;
+
+            lock (_tasks)
+            {
+                while (_tasks.Count == 0)
+                {
+                    Monitor.Wait(_tasks);
+                }
+
+                task = _tasks.Dequeue();
+            }
+
+            task.Start();
+        }
+
+        private void Run()
+        {
+            try
+            {
+                while ((Thread.CurrentThread.ThreadState & ThreadState.WaitSleepJoin) == 0)
+                {
+                    Eval();
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+            }
+            finally
+            {
+                Thread.CurrentThread.Interrupt();
+            }
+        }
+
+        public void Dispose()
+        {
+            _worker.Interrupt();
+
+            try
+            {
+                _worker.Join();
+            }
+            catch (ThreadInterruptedException)
+            {
+            }
+        }
     }
 }
-
