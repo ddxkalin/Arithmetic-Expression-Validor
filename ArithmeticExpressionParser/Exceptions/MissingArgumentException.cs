9b820ab0f53fc6a05ab88023d2d0b3d4fb56141e
Kalin Stoev <kstoev9316@gmail.com>
2022-10-04 22:55:18 +0300

    Added Custom Exceptions in the Parser

---
+++ ArithmeticExpressionParser/Exceptions/MissingArgumentException.cs
@@ -0,0 +1,19 @@
+﻿namespace ArithmeticExpressionParser.Exceptions
+{
+    public class MissingArgumentException : ParsingException
+    {
+        public MissingArgumentException(string expression, int ind) :
+            base("Missing argument: ", expression, ind)
+        {
+        }
+
+        public override string ErrorDescription()
+        {
+            var res = "";
+            res += Expression.Substring(0, ErrorIndex);
+            res += "{$}";
+            res += Expression.Substring(ErrorIndex);
+            return res;
+        }
+    }
+}
---
+++ ArithmeticExpressionParser/Exceptions/ParsingException.cs
@@ -0,0 +1,16 @@
+﻿namespace ArithmeticExpressionParser.Exceptions
+{
+    public abstract class ParsingException : Exception
+    {
+        protected readonly int ErrorIndex;
+        protected readonly string Expression;
+
+        protected ParsingException(string message, string expression, int ind) : base(message)
+        {
+            ErrorIndex = ind;
+            Expression = expression;
+        }
+
+        public abstract string ErrorDescription();
+    }
+}
